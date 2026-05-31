003487b785c17835a8ded4a44cc68013e55ee545
Shashikant Sutar <svs.sutar@gmail.com>
2026-05-31 12:13:05 +0530

    Modified following files pages/homePage.ts, leadpage.ts, tests/lead.spec.ts and added lead.json file

--- tests/lead.spec.ts
+++ tests/lead.spec.ts
@@ -2,22 +2,41 @@ import { expect, test } from "@playwright/test";
 import { LoginPage } from "../pages/loginPage";
 import { HomePage } from "../pages/homePage";
 import { LeadPage } from "../pages/leadPage";
+import { getTestData } from "../utilities/jsonReader";
 
 
 
 let loginPage: LoginPage;
 let homePage: HomePage;
+let leadPage: LeadPage;
 
 test('Create_New_Lead_with_Mandatory_Fields_TC_001', async({page})=>{
     loginPage = new LoginPage(page);
     const leadPage = new LeadPage(page);
-    homePage = new HomePage(page);
+    const homePage = new HomePage(page);
     await page.goto('/');
-    await loginPage.login("admin","admin");
+    const testData = await getTestData('./testdata/leads.json','Create_New_Lead_with_Mandatory_Fields_TC_001')
+    await loginPage.login(testData.username,testData.password);
     await homePage.clickNewLead();
-    await leadPage.saveLead("Vishwanth", "Axis");
+    await leadPage.saveLead(testData.lastname, testData.company);
     const isLastNameDisplayed = await leadPage.isLastNameDisplayed();
     expect(isLastNameDisplayed).toBe(true);
     const isCompanyDisplayed = await leadPage.isCompanyDisplayed();
     expect(isCompanyDisplayed).toBe(true);
+})
+
+test('Verify_Search_Existing_Lead_by_Lastname_TC002', async ({ page }) => {
+
+    loginPage = new LoginPage(page);
+    homePage = new HomePage(page);
+    leadPage = new LeadPage(page);
+    await page.goto('/');
+    const testData = await getTestData('./testdata/leads.json', 'Verify_Search_Existing_Lead_by_Lastname_TC002')
+    await loginPage.login(testData.username, testData.password);
+    await homePage.clickLeads();
+    await leadPage.searchLeadLastName(testData.lastname)
+    await leadPage.clickSearch();
+    const name = await leadPage.isLastNameDisplayed()
+    expect(name).toBe(true);
+    await page.close();
 })
