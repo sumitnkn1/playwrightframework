ac9cfda3364e95fa5b3bd0ed15de0f59fb4bb75b
Shashikant Sutar <svs.sutar@gmail.com>
2026-06-07 13:05:32 +0530

    Refractored login.spec.ts to comsider test only related to vTiger CRM deleted unrelated file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -70,70 +70,7 @@ await page.locator("//input[@title='Search [Alt+Q]']").click();
 await page.locator("//input[@type='checkbox']").click();
 await page.locator("//input[@value='Delete']").click();
 
-
 });
-
-
-test('google', async ({ page }) => {
- 
-await page.goto('http://google.com');
-await page.waitForTimeout(5000);
-await page.locator("//textarea[@class='gLFyf']").fill('Automation test hub');
-await page.waitForTimeout(5000);
-//await page.locator("//input[@name='user_name']").fill('admin');
-//await page.locator("//input[@name='user_password']").fill('admin');
-await page.locator("//span[@class='lTxWLe']").press('Enter');
-await page.locator("//div[@class='recaptcha-checkbox-border']").click();
-await page.waitForTimeout(30000);
-await page.locator("//span[@id='recaptcha-anchor']").check();
-await page.close();
-
-//.press('Enter');
-
-
-});
-
-
-test('Amazon', async ({ page }) => {
- 
-await page.goto('https://www.amazon.in/');
-await page.locator("//div[@class='nav-div' and @id='nav-link-accountList']").click();
-await page.locator("//input[@name='email']").fill('9805214869');
-await page.locator("//input[@type='submit']").click();
-//await expect(page).toHaveTitle(/Amazon/);
-await page.waitForTimeout(5000);
-await page.close();
-});
-
-
-test('New', async ({ page }) => {
- 
-await page.goto('https://www.amazon.in/');
-//await page.locator("button[type='submit']").click();
-await page.locator("//div[@class='nav-div' and @id='nav-link-accountList']").click();
-await page.locator("//input[@name='email']").fill('7018287926');
-await page.locator("input[type='submit']").click();
-await page.locator("//input[@type='password']").fill('Eshu@1234#');
-//await expect(page).toHaveTitle(/Amazon/);
-await page.waitForTimeout(5000);
-await page.locator("//a[contains(text(),'Mobiles' )]").click();
-await page.locator("//input[@id='apb-browse-refinements-checkbox_1']").check();
-await expect(page.getByRole('heading', { name: /Samsung Galaxy M07 Mobile/ })).toBeChecked();
-
-await page.close();
-});
-
-test('google search', async ({ page }) => {
- 
-await page.goto('https://www.google.com');
-await page.locator("//div[@class='nav-div' and @id='nav-link-accountList']").click();
-await page.locator("//input[@name='email']").fill('9805214869');
-await page.locator("input[name='Login']]").click();
-//await expect(page).toHaveTitle(/Amazon/);
-await page.waitForTimeout(5000);
-await page.close();
-});
-
 
 
 
